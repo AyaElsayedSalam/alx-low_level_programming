4d63a585eb61526a45a90a54374845ffb176c78d
mshaker <mahm231@gmail.com>
2023-08-10 21:09:13 +0200

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,8 @@
-#include <unistd.h> 
+#include <unistd.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
 fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return 1;
+return (1);
 }
