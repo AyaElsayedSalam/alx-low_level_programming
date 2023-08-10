6c4913593c8287fd1c3e8af5d2e24ef90e4daf0d
mshaker <mahm231@gmail.com>
2023-08-10 21:07:33 +0200

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,8 @@
 #include <unistd.h> 
-#include <sys/syscall.h> 
 #include <stdio.h>
+
 int main(void) 
 {
-const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(STDOUT_FILENO,msg, sizeof(msg));
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return 1;
 }
