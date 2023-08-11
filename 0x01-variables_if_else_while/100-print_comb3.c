e552c9518c88a905a14bbf156f42037752a1a7b5
mshaker <mahm231@gmail.com>
2023-08-11 20:47:02 +0200

    advanced task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,7 +8,7 @@ int main(void)
 int num1, num2;
 for (num1 = 0 ; num1 < 10 ; num1++)
 {
-for (num2 = 0 ; num2 <10 ; num2++)
+for (num2 = 0 ; num2 < 10 ; num2++)
 {
 if ((num1 >= num2) || ((num1 == 1) && (num2 == 0)))
 continue;
@@ -18,7 +18,7 @@ putchar('0' + num1);
 putchar('0' + num2);
 }
 
-if ((num1 == 8) && (num2 == 9)) 
+if ((num1 == 8) && (num2 == 9))
 continue;
 else
 {
