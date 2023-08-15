7297958bf77b26d71e2c4debe94f0143e348033d
mshaker <mahm231@gmail.com>
2023-08-15 16:06:30 +0200

    0-putchar task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "_putchar.h"
+/**
+* main - Entry point
+ * Return: Always 0
+*/
+int main(void)
+{
+int i;
+char s[] ={"_putchar"};
+for(i =0; i < (int)sizeof(s) ; i++)
+_putchar(s[i]);
+
+_putchar('\n');
+return (0);
+}
