2ccfd17f622c0df73eaaca411206ce61fcd5a0a7
mshaker <mahm231@gmail.com>
2023-08-16 11:53:33 +0200

    0x02. C - Functions, nested loop advanced task part 3

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+  * main - print the first 50 fibonacci numbers.
+  * Return: void.
+  */
+int main(void)
+{
+unsigned long count, i, j, m;
+i = 0;
+j = 1;
+for (count = 0; count < 98; count++)
+{
+	m = i + j;
+	i = j;
+	j = m;
+	printf("%lu", m);
+	if (count == 97)
+		putchar('\n');
+	else
+	{
+		printf(", ");
+	}
+}
+return (0);
+}
