792bd1c1a962b66c02787d7b173ca513706135fb
mshaker <mahm231@gmail.com>
2023-08-18 13:40:46 +0200

    0x04. C - More functions, more nested loops part1

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,12 +15,9 @@ while (i < times_of_print)
 for (n = 0; n < 15; n++)
 {
 if (n > 9)
-{
 _putchar((n / 10) + '0');
+
 _putchar((n % 10) + '0');
-}
-else
-_putchar(n + '0');
 }
 _putchar('\n');
 i++;
