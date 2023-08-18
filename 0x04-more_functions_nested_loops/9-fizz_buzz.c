a4898129f122e3eeace993b2d8066b8586ec9369
mshaker <mahm231@gmail.com>
2023-08-18 14:28:11 +0200

    0x04. C - More functions, more nested loops part2 fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,13 +11,13 @@ int i;
 for (i = 1 ; i <= 100 ; i++)
 {
 if (((i % 3) == 0) && ((i % 5) == 0))
-printf("%s ", "FizzBuzz");
+printf("FizzBuzz ");
 
 else if ((i % 5) == 0)
-printf("%s ", "Buzz");
+printf("Buzz ");
 
 else if ((i % 3) == 0)
-printf("%s ", "Fizz");
+printf("Fizz ");
 
 else
 printf("%d ", i);
