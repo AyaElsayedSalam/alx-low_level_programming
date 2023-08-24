2e37e41ef159d28ab7cade0d5d77e446fd47ea71
mshaker <mahm231@gmail.com>
2023-08-24 21:39:56 +0200

    pointers part2

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,12 +7,11 @@
 */
 void reverse_array(int *a, int n)
 {
-int i = 0, j = n -1, temp;
-for (i = 0 ; i < n/2 ; i++)
+int i = 0, j = n - 1, temp;
+for (i = 0 ; i < (n / 2) ; i++)
 {
 temp = a[i];
 a[i] = a[j];
 a[j--] = temp;
 }
-
 }
