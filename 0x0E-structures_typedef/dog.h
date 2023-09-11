82752651ef6bdf729036a5e7e1b54a304c1cfda3
mshaker <mahm231@gmail.com>
2023-09-11 17:52:30 +0200

    typdef 4

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,13 +8,13 @@
  *
  * Description: Longer description
  */
-struct dog
+typedef struct dog
 {
     /* data */
 char  *name;
 float age;
 char *owner;
-};
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
