076249437b253bac8215892404f8fcee9a69e35e
mshaker <mahm231@gmail.com>
2023-09-11 17:22:59 +0200

    typdef 1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - define parameters of struct dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: the dog owner
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+    /* data */
+char  *name;
+float age;
+char *owner;
+};
+
+#endif /* DOG_H */
