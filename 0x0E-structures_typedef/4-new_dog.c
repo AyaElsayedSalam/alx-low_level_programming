5b54660a42e7ee7d36042a4a835afb9337363e16
mshaker <mahm231@gmail.com>
2023-09-11 21:39:05 +0200

    typdef 5

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,38 +10,38 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *n_dog;
-char *cname, *cowner;
-int len_name = 0, len_owner = 0, i = 0;
-if ((name == NULL) || (owner == NULL))
-return (NULL);
-n_dog = malloc(sizeof(dog_t));
-if (n_dog == NULL)
-return (NULL);
-while (name[len_name])
-len_name++;
-while (owner[len_owner])
-len_owner++;
-cname = malloc(len_name + 1);
-cowner = malloc(len_owner + 1);
-if (cname == NULL)
-return (NULL);
+	dog_t *new_dog;
+	char *cpyname, *cpyowner;
+	int len_name = 0, len_owner = 0, i;
 
-if (cowner == NULL)
-return (NULL);
+	if (name == NULL || owner == NULL)
+		return (NULL);
 
-for (i = 0 ; i < len_name ; i++)
-{
-cname[i] = name[i];
-}
-cname[i] = '\0';
-for (i = 0; i < len_owner ; i++)
-{
-cowner[i] = owner[i];
-}
-cowner[i] = '\0';
-n_dog->age = age;
-n_dog->name = cname;
-n_dog->owner = cowner;
-return (n_dog);
+	while (name[len_name])
+		len_name++;
+	while (owner[len_owner])
+		len_owner++;
+
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+
+	cpyname = malloc(len_name + 1);
+	if (cpyname == NULL)
+		return (NULL);
+	for (i = 0; name[i]; i++)
+		cpyname[i] = name[i];
+	cpyname[i] = '\0';
+
+	cpyowner = malloc(len_owner + 1);
+	if (cpyowner == NULL)
+		return (NULL);
+	for (i = 0; owner[i]; i++)
+		cpyowner[i] = owner[i];
+	cpyowner[i] = '\0';
+
+	new_dog->name = cpyname;
+	new_dog->age = age;
+	new_dog->owner = cpyowner;
+	return (new_dog);
 }
