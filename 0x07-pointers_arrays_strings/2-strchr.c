06946c672c8c5f16bdaf21b2f9c02f70a939cfda
mshaker <mahm231@gmail.com>
2023-09-12 17:43:13 +0200

    test

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,8 +9,7 @@
 char *_strchr(char *s, char c)
 {
 int i = 0, len = 0;
-if (*s == c)
-return (s);
+
 
 while (s[len] != '\0')
 {
@@ -21,5 +20,8 @@ for (i = 0 ; i < len ; i++)
 if (s[i] == c)
 return (s + i);
 }
-return (NULL);
+if (*s == c)
+return (s);
+else
+return (0);
 }
