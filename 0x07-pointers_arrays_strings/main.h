77302e3acdd21fb2bd93dc93711d54bebe7417a1
mshaker <mahm231@gmail.com>
2023-09-13 14:31:30 +0200

    chessboard

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,4 +8,5 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 int checkSubstring (char *h, char *n);
+void print_chessboard(char (*a)[8]);
 #endif /* MAIN_H */
