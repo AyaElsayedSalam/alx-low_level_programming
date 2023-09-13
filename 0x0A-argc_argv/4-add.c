8161f89e82bfd4faa991b99e31d25021a497557a
mshaker <mahm231@gmail.com>
2023-09-13 15:26:57 +0200

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that adds positive numbers.
+ * @argc: number of arguments
+ * @argv: array with the arguments
+ *
+ * Return: always 0
+ **/
+int main(int argc, char *argv[])
+{
+int add, i;
+if (argc == 1)
+{
+printf("%d\n", 0);
+return (0);
+}
+for (i = 1 ; i < argc ; i++)
+{
+if ((*(argv[i]) > 47) && (*(argv[i]) < 57))
+add += atoi(argv[i]);
+
+else
+{
+printf("%s\n", "Error");
+return (1);
+}
+}
+printf("%i\n", add);
+return (0);
+}
